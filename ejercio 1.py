34e143406496b0a7e4a33dc3705ead1f74478561
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-08-26 12:37:36 -0300

    Add files via upload

---
+++ ejercio 1.py
@@ -0,0 +1,16 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=3
+>>> y=2
+>>> x*y
+6
+>>> nombre= input("ingrese su nombre")
+ingrese su nombre Maria Paula Zurron
+>>> edad= input("ingrese su edad")
+ingrese su edad 30
+>>> print(f"el nombre del usuario es {nombre}")
+el nombre del usuario es  Maria Paula Zurron
+>>> print(f"la edad es {edad}")
+la edad es  30
+>>> if--name--"--main--":
+	main()

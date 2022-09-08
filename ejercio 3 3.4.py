cad5771b7f572b2520b095cda65bff797e6c2f9b
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-07 23:13:37 -0300

    Add files via upload

---
+++ ejercio 3 3.4.py
@@ -0,0 +1,6 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def intermedio(num1, num2):
+    return (num1 + num2) / 2
+
+intermedio(-12, 24)

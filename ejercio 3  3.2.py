83173adf7e328b69abe48422d68b3ee28779a02d
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-07 23:01:42 -0300

    Add files via upload

---
+++ ejercio 3  3.2.py
@@ -0,0 +1,16 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+print(math.pi)
+3.14159
+from math import pi
+def area_circulo(radio):
+        return pi*radio**2
+
+
+
+
+
+
+
+ 

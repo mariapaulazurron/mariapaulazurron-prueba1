51f89bdb7e60795bc11f3d5a71cb82e25356270b
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-07 22:50:32 -0300

    Add files via upload

---
+++ ejercio3 3.1.py
@@ -0,0 +1,6 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def area_rectangulo(base,altura):
+	return base*altura
+
+area_rectangulo(15,10)

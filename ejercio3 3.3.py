2d56fb01dfc24fd442d66848f4f5e86385606bbf
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-07 23:02:47 -0300

    Add files via upload

---
+++ ejercio3 3.3.py
@@ -0,0 +1,11 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def relacion(num1, num2):
+        if num1>num2:
+                return 1
+        elif num1<num2:
+                return -1
+        return 0
+	
+
+print(relacion(5,10), relacion(10,5),relacion (5,5))

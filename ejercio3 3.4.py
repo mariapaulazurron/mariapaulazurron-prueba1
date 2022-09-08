65707fa578c83f55428578b5723c50585a53b674
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-07 23:03:34 -0300

    Add files via upload

---
+++ ejercio3 3.4.py
@@ -0,0 +1,13 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def intermedio():
+	x= int(input("digite el primer numero:"))
+	y= int(input("digite el segundo numero:"))
+	z= int(input("digite el tercer numero:"))
+
+	
+>>> def intermedio():
+	menor= min(x,y,z)
+	mayor= max(x,y,z)
+	valor_medio=(x+y+z)-min-max
+	print(menor, valor_medio,mayor)

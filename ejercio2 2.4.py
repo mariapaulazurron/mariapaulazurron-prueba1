29465fa33197156dc442064a498c30778bb4165c
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-14 23:54:23 -0300

    Add files via upload

---
+++ ejercio2 2.4.py
@@ -0,0 +1,13 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def main():
+        lista[1,2,3,4]
+def get_number_of_elementos(lista):
+                contador=0
+                for elementos in lista:
+                        ontador+=1
+                        return cantidad
+                print("numeros de elementos en la la lista:",get_number_of_elementos)
+
+
+       

e9e9b565365a194a1fca37dac24c827f09a76fa0
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-14 23:50:10 -0300

    Add files via upload

---
+++ Ejercio 2 2.1.py
@@ -0,0 +1,16 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>def main():
+    print("PARES E IMPARES")
+    numero_1 = int(input("Escriba un número entero: "))
+    numero_2 = int(input(f"Escriba un número entero mayor o igual que {numero_1}: "))
+
+    if numero_2 < numero_1:
+        print(f"¡Le he pedido un número entero mayor o igual que {numero_1}!")
+    else:
+        for i in range(numero_1, numero_2 + 1):
+            if i % 2 == 0:
+                print(f"El número {i} es par.")
+            else:
+                print(f"El número {i} es impar.") 
+                

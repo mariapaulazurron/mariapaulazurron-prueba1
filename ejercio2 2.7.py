bfb50e401fa24cd7dfe383eb09f64cc8973afa1f
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-14 23:56:31 -0300

    Add files via upload

---
+++ ejercio2 2.7.py
@@ -0,0 +1,12 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def main():
+    lista[ 1,8,10,20,90,40]
+    numeromax=lista[0]
+    for numeroactual in lista:
+        if numeroactual>numeromax:
+            numeromax=numeroactual
+            print(f"el mayor numero de la lista es:{numeromax}")
+
+
+	

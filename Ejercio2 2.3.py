63c8342311d80d6be324e0c9bf8c0a1b2fd076ed
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-14 23:53:18 -0300

    Add files via upload

---
+++ Ejercio2 2.3.py
@@ -0,0 +1,14 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def main():
+	mail=input("ingrese su mail:")
+	correo="mariapaula.zurron@hotmail.com"
+	if "@" in correo:
+                print("es valido")
+                
+                        
+
+
+
+		
+

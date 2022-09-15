0d826b03f49c1fd8bef2a430146b9ec0613a7a18
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-14 23:55:54 -0300

    Add files via upload

---
+++ Ejercio2 2.6.py
@@ -0,0 +1,24 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def main():
+       lista=[2,4,3]
+       contador=0
+       suma=0
+       for elemento in lista:
+               suma=suma+elemento
+               contador=contador+1
+               promedio=suma/contador
+               print(f"elpromedio es:{promedio}")
+               
+                
+
+
+
+
+
+                                        
+                                       
+                                        
+                                        
+		
+

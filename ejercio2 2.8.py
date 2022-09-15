222c62cf9fd9c1564a8d1c5542117c650fdfe7e8
mariapaulazurron <111582313+mariapaulazurron@users.noreply.github.com>
2022-09-14 23:59:02 -0300

    Add files via upload

---
+++ ejercio2 2.8.py
@@ -0,0 +1,14 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>def main():
+    lista[12,1,78,45]
+    numeromin = lista[0]
+    for numeroactual in lista:
+        if numeroactual < numeromin:
+            numeromin=numeroactual
+            print(f"elmenor numero de la lista es:{numeromin}")
+             
+                               	
+
+
+
